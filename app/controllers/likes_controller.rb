e4f338f9881b4a821601c85cb456f88c195b4f06
myao04 <web.info04s@gmail.com>
2018-05-23 12:10:02 +0000

    kadai repair

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,18 @@
 class LikesController < ApplicationController
+  before_action :require_user_logged_in
+  
   def create
-    user = User.find(params[:micropost_id])
-    current_user.favorite(user)
+    user = User.find(current_user)
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.favorite(micropost)
     flash[:success] = '投稿をお気に入りしました。'
-    redirect_to user  
+    redirect_to user
   end
 
   def destroy
-    user = User.find(params[:micropost_id])
-    current_user.unfavorite(user)
+    user = User.find(current_user)
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.unfavorite(micropost)
     flash[:success] = '投稿のお気に入りを解除しました。'
     redirect_to user
   end
